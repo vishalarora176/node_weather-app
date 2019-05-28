b29b9e6c852017295512c5f06086dda0cf5790df
Vishal Arora <Vishalarora@MPB021-VISHALARORA.local>
2019-05-28 23:01:54 +0530

    remove the console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,10 @@
-console.log('client side javascript is loaded !')
-
 const weatherForm = document.querySelector('form')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (event) => {
   event.preventDefault()
-const location = document.querySelector('input').value
+  const location = document.querySelector('input').value
   fetch('http://localhost:3000/weather?address=' + location).then(response => {
     response.json()
     .then(parsedData => {
