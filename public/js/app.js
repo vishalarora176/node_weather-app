83c9eb6880dd227f4e46192263db6cbecf9cf527
Vishal Arora <Vishalarora@MPB021-VISHALARORA.local>
2019-05-29 20:00:57 +0530

    changes for adding the code to production

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (event) => {
   event.preventDefault()
   const location = document.querySelector('input').value
-  fetch('http://localhost:3000/weather?address=' + location).then(response => {
+  fetch('/weather?address=' + location).then(response => {
     response.json()
     .then(parsedData => {
       if (parsedData.error) {
@@ -14,7 +14,6 @@ weatherForm.addEventListener('submit', (event) => {
       } else {
         messageOne.textContent = parsedData.location
         messageTwo.textContent = parsedData.forecast
-
       }
     })
   })
